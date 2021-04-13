bc96dc737b3939d24122f54789910aa4c8c3b777
ALcot <mbaaccarima@gmail.com>
2021-04-13 21:42:10 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+class Main {
+  public static void main(String[] args) {
+    // 条件式に直接trueをいれる
+    if (true) {
+      System.out.println("条件式がtrueのため、出力される");
+    }
+    
+    // 条件式に直接falseをいれる
+    if (false) {
+      System.out.println("条件式がfalseのため、出力されない");
+    }
+    
+    int x = 5;
+    // if文を用いて、変数xが2より大きいとき「xは2より大きい」と出力する
+    if (x>2){
+      System.out.println("xは2より大きい");
+    }
+    
+    // if文を用いて、変数xが8より大きいとき「xは8より大きい」と出力する
+    if (x>8){
+      System.out.println("xは8より大きい");
+    }
+    
+  }
+}
